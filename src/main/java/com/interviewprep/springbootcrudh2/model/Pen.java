4407a3950da6bd9e7aba19abe44e97e3eeb0d868
admin <ma.kashalikar@gmail.com>
2022-01-11 18:50:09 +0530

    code added to file

--- src/main/java/com/interviewprep/springbootcrudh2/model/Pen.java
+++ src/main/java/com/interviewprep/springbootcrudh2/model/Pen.java
@@ -1,1 +1,21 @@
+public class Pen{
 
+	private long id;
+	private String brand;
+	private String color;
+
+	public Pen(long id,String brand,String color){
+		this.id = id;
+		this.brand = brand;
+		this.color = color;
+	}
+
+	public long getId(){
+		return id;	
+	}
+
+	public void setId(long id){
+		this.id = id;
+	}
+	
+}
