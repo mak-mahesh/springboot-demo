779fdd15a2d0407f90d5cf0904a719ba04a5a52b
admin <ma.kashalikar@gmail.com>
2022-04-08 23:55:47 +0530

    added file

--- src/main/java/com/interviewprep/springbootcrudh2/repository/BooksRepository.java
+++ src/main/java/com/interviewprep/springbootcrudh2/repository/BooksRepository.java
@@ -6,5 +6,6 @@ import org.springframework.stereotype.Repository;
 import com.interviewprep.springbootcrudh2.model.Books;
 @Repository
 public interface BooksRepository extends CrudRepository<Books, Integer>  
-{  
+{ 
+	
 } 
